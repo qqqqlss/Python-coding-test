60e074fe63e9af1be29ff0b6a59b5f9cf0f8fd8a
Lee do won <qqqqlss@naver.com>
2021-12-24 20:31:48 +0900

    programmers kit hash_1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Python-coding-test
+use dictionary for hash in python
+hash_1 - 딕셔너리 이용해 hash구분  key,value,items
+hash_2 - for 문에서 딕셔너리가 리스트 보다 빠름 ! 
---
+++ hash_1.py
@@ -0,0 +1,15 @@
+def solution(participant, completion):
+    hs = dict()
+    for i in participant:
+        if i not in hs:
+            hs[i] = 1
+        else:
+            hs[i] +=1
+            
+        
+    for i in completion:
+        hs[i] -=1
+    
+    for key ,val in hs.items():
+        if val != 0:
+            return key
