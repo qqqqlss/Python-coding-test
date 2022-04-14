315dc092898999c10c8df137268f0b9d4f626490
Lee do won <qqqqlss@naver.com>
2022-04-14 11:04:29 +0900

    solve greedy_6.py

---
+++ greedy_6.py
@@ -0,0 +1,13 @@
+def solution(routes):
+    for i in routes:
+        i.sort()
+    routes.sort()
+    answer=1
+    point=routes[0][1]
+    for i in routes[1:]:
+        if i[1]<point:
+            point=i[1]
+        elif i[0]>point:
+            point=i[1]
+            answer+=1
+    return answer
