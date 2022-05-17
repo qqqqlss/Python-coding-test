290b89e01d32d03f7c7b228858de90ec711205af
Lee do won <qqqqlss@naver.com>
2022-05-17 11:18:08 +0900

    solve dynamic_programming_4

---
+++ dynamic_programming_4.py
@@ -0,0 +1,7 @@
+def solution(a):
+    x1, y1, z1 = a[0], a[1], a[0]+a[2]
+    x2, y2, z2 = 0, a[1], a[2]
+    for i in a[3:]:
+        x1, y1, z1 = y1, z1, max(x1, y1)+i
+        x2, y2, z2 = y2, z2, max(x2, y2)+i
+    return max(x1, y1, y2, z2)
