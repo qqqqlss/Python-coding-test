ecdbd360f9619ce8177532eb189bdb089917e8e0
Lee dowon <qqqqlss@naver.com>
2022-12-11 18:04:27 +0900

    solve 가장 가까운 글자.py

---
+++ Lv1/README.md
@@ -0,0 +1,6 @@
+# Python-coding-test
+---
+=======
+
+**Level 1**   
+가장 가까운 글자 - hash 사용  
---
+++ Lv1/가장 가까운 글자.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = []
+    d=dict()
+    for i in range(len(s)):
+        if s[i] in d:
+            answer.append(i-d[s[i]])
+        else:
+            answer.append(-1)
+        d[s[i]]=i
+    return answer
